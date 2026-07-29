7fce216d861c1891967c734be1095f5262498834
SFrostStar <dmitriyskin99@gmail.com>
2026-07-29 04:07:52 +0300

    fix(SkyBrowser): use file:// protocol for WKWebView full origin permissions in Rust browser

--- SkyBrowser/src/main.rs
+++ SkyBrowser/src/main.rs
@@ -13,19 +13,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_min_inner_size(tao::dpi::LogicalSize::new(800.0, 500.0))
         .build(&event_loop)?;
 
-    let html_content = include_str!("../index.html");
+    let current_dir = std::env::current_dir()?;
+    let index_path = current_dir.join("index.html");
+    let file_url = format!("file://{}", index_path.to_string_lossy());
+    
     let user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36";
 
     let _webview = WebViewBuilder::new(&window)
         .with_user_agent(user_agent)
-        .with_html(html_content)
+        .with_url(&file_url)
         .build()?;
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
         match event {
-            Event::NewEvents(StartCause::Init) => println!("[Sky Rust] Multi-Tab Native Browser started successfully."),
+            Event::NewEvents(StartCause::Init) => println!("[Sky Rust] Multi-Tab Native Browser started cleanly via local protocol."),
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 ..
